004cc79a75f616a1d8c4cbf097fa8fc1d0599988
Thomas Harvey <thomasharvey24@gmail.com>
2015-06-26 19:56:41 -0400

    Word Model can now access access database

--- models/words.js
+++ models/words.js
@@ -1,15 +1,38 @@
 var db = require("../db");
 
 exports.all = function(cb){
-    var collection = db.collection("words");
-    collection.find().toArray(function(err,docs){
-        cb(err, docs);
+    var cursor = db.collection("words");
+    cursor.find().toArray(function(err,docs){
+        if(err){
+            cb(err);
+        }
+        else{
+            cb(null, docs);
+        }
     });
 }
 
-exports.startsWith = function(cb){
-    var collection = db.get().collection("words");
-    collection.find().sort({"date": -1}).limit(100).toArray(function(err, docs){
-        cb(err, docs);
+exports.startsWith = function(letters,cb){
+    var letterRegex = "";
+    var limit = letters.length;
+    for(i=0; i<limit;i++){
+        if (i + 1 == letters.length){
+            letterRegex += letters[i];
+        }
+        else{
+            letterRegex += letters[i] + "|";
+        }
+    }
+    var r = '^(' + letterRegex + ')';
+    // ------------------------------------------
+
+    var cursor = db.collection("words");
+    cursor.find({'word':{$regex: r, $options: 'i'}}).toArray(function(err,docs){
+        if(err){
+            cb(err);
+        }
+        else{
+            cb(null, docs);
+        }
     });
 }
--- server.js
+++ server.js
@@ -4,6 +4,13 @@ http = require('http').Server(app),
 io = require('socket.io')(http),
 socketManagement = require('./user_socket');
 
+// Not Sure If I am supposed to do this Here
+// But since it just Exports the Functions 
+// The Connection Should remain unchanged
+var db = require("./db");
+db.connect('mongodb://localhost:27017/gameServer',function(){
+    console.log("Mongo Connected.");
+});
 // Make this work
 // Nice
 socketManagement.live(io);
@@ -13,6 +20,8 @@ app.use(express.static(__dirname + '/public'));
 var gameRouter = require('./controllers/boards/index');
 app.use('/game', gameRouter);
 
+var wordRouter = require('./controllers/words/index');
+app.use('/words', wordRouter);
 // var mainRouter = require('./controllers/main/index');
 // app.use(mainRouter);
 
